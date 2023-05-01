8aa357e504ce4f0ff5524281784dc49b94132763
s4nxez@gmailcom <cobra_leina@hotmail.es>
2023-05-01 22:46:58 +0200

    Plantilla Dao hecha y service empezada

--- src/main/java/org/example/dao/UserDao.java
+++ src/main/java/org/example/dao/UserDao.java
@@ -1,4 +1,15 @@
 package org.example.dao;
 
-public class UserDao {
+import org.example.domain.User;
+
+import java.util.ArrayList;
+
+public class UserDao extends User {
+    ArrayList<User> Users = new ArrayList();
+    private boolean guardar(User user){
+        return ;
+    }
+    private User findByUserName(String userName){
+        return ;
+    }
 }
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -1,4 +1,13 @@
 package org.example.service;
 
+import org.example.dao.UserDao;
+
 public class UserService {
+    UserDao userDao = new UserDao();
+    private boolean login(String username, String pwd){
+        if (userDao.Username.equals(username)&&userDao.pwd.equals(pwd)){
+            return true;
+        }
+        else return false;
+    }
 }
