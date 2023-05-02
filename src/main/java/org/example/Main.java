716859c22da60b3ab299d2e080f6f35acf2bdc22
s4nxez@gmailcom <cobra_leina@hotmail.es>
2023-05-02 02:15:14 +0200

    Registro empezado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,44 @@
 package org.example;
 
+import org.example.domain.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("12");
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<User> comunidad = new ArrayList();
+        // Esto es solo para el user
+        System.out.println("Bienvenido, selecciona una opción:");
+        System.out.println("1.Iniciar sesión");
+        System.out.println("2.Registrarse");
+
+        int o = sc.nextInt();
+
+        switch (o){
+            case 1:
+                break;
+            case 2:
+                System.out.println("---------------------");
+                System.out.println("Registro");
+                System.out.println("---------------------");
+                  System.out.print("Nombre: ");String nombre= sc.nextLine();
+                  System.out.print("Email: ");String email= sc.nextLine();
+                  System.out.print("Dirección: ");String direccion=sc.nextLine();
+                  System.out.print("Fecha Nacimiento: ");
+                  System.out.print("             Año: ");int anyo= sc.nextInt();
+                  System.out.print("             Mes: ");int mes= sc.nextInt();//mes--;
+                  System.out.print("             Día: ");int dia= sc.nextInt();
+                  LocalDate fechaNac= LocalDate.of(anyo,mes,dia);
+                  System.out.print("Nombre de usuario: ");String usrName= sc.nextLine();
+                  System.out.print("Contraseña: ");String pwd= sc.nextLine();
+                User user= new User(nombre,email,direccion,fechaNac,usrName,pwd);
+                comunidad.add(user);
+                break;
+        }
 
     }
 }
--- src/main/java/org/example/domain/User.java
+++ src/main/java/org/example/domain/User.java
@@ -1,12 +1,17 @@
 package org.example.domain;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class User {
-    String name, email, direccion, fecha_nac, username, pwd, friends;
+    String name, email, direccion, username, pwd;
+    LocalDate fecha_nac = LocalDate.now();
+
 
     public boolean sendPackage(Package Package, String nombreReceptor){
         return;
     }
-    private String getEstadoPaquete(String paquete_id){// este metodo igual nos colamos en el DC al ponerlo aqui
+    private String getEstadoPaquete(String paquete_id){// este metodo igual nos colamos en el DC al ponerlo aqui creo que tendria que estar en paquete en la carpeta domain
         return ;
     }
     private boolean addFriend(User amigo){
@@ -16,4 +21,13 @@ public class User {
         return ;
     }
 
+    public User(String name, String email, String direccion, LocalDate fecha_nac, String username, String pwd) {
+        this.name = name;
+        this.email = email;
+        this.direccion = direccion;
+        this.fecha_nac = fecha_nac;
+        this.username = username;
+        this.pwd = pwd;
+        ArrayList<User> Friends = new ArrayList();
+    }
 }
